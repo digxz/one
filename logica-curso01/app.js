80871e256cc79a9aa08dd5ae8e6921034a861ea7
digxz <diegocastelliniramirez.dcr@gmail.com>
2024-07-09 22:24:40 -0300

    Last commit

--- logica-curso01/app.js
+++ logica-curso01/app.js
@@ -1,23 +1,28 @@
 alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 5;
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 let chute;
 let tentativas = 1;
 
 // while
 while (chute != numeroSecreto) {
-  chute = prompt("Digite um número entre 0 e 10");
+  chute = prompt(`Digite um número entre 1 e ${numeroMaximo} `);
 
   // if and else
   if (chute == numeroSecreto) {
-    alert(
-      `Parabéns, você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas`
-    );
+    break;
   } else {
     if (chute > numeroSecreto) {
       alert(`O número secreto é menor que o chute ${chute}`);
     } else {
       alert(`O número secreto é maior que o chute ${chute}`);
     }
+
     tentativas++; //tentativas = tentativas + 1
   }
 }
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa"; //Operador ternário
+alert(
+  `Parabéns, você acertou o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.	`
+);
